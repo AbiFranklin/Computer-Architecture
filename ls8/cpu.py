55786a2fd558d55c8a9f0066a8b1074e36d3f2a8
Abigayle Franklin <abifanklin@gmail.com>
2019-08-24 17:03:57 -0500

    HLT implemented

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -69,6 +69,12 @@ class CPU:
 
     def run(self):
         running = True
+        LDI = 0b10000010
+        PRN = 0b01000111
+        HLT = 0b00000001
+        MUL = 0b10100010
         while running:
             IR = self.ram[self.pc]
+            if IR == HLT:
+                running = False
 

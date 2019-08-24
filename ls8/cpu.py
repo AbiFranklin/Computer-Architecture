5d6a00bcde58219b93177de4d71ebad3d280d936
Abigayle Franklin <abifanklin@gmail.com>
2019-08-24 18:10:28 -0500

    MUL implemented and tested. Day 2 complete.

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -17,25 +17,7 @@ class CPU:
         self.ram[MAR] = MDR
 
     def load(self):
-        """Load a program into memory."""
-
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000001,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
         
         try:
             with open(sys.argv[1]) as file:
@@ -51,11 +33,11 @@ class CPU:
 
 
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
-
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
+        # elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -85,11 +67,12 @@ class CPU:
         PRN = 0b01000111
         HLT = 0b00000001
         MUL = 0b10100010
-        operand_a = self.ram_read(self.pc + 1)
-        operand_b = self.ram_read(self.pc + 2)
+        
 
         while running:
             IR = self.ram[self.pc]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
             if IR == HLT:
                 running = False
             elif IR == LDI:
@@ -98,6 +81,9 @@ class CPU:
             elif IR == PRN:
                 print(self.reg[operand_a])
                 self.pc += 2
+            elif IR == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
             else:
                 print('Error')
                 sys.exit()
